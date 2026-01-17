6b6ba1124bb3b0487f64e1f4cab8c8db06ea7ad5
Parthshrivastava007 <parth,dollor@gmail.com>
2026-01-18 01:34:06 +0530

    Refactor user routes in app.js and clean up commented code

--- src/app.js
+++ src/app.js
@@ -2,21 +2,33 @@ const express = require("express");
 
 const app = express();
 
+// app.use("/", (req, res) => {
+//   res.send("Hello!")
+// })
+
+app.get("/user", (req, res) => {
+  res.send({ firstname: "Parth", lastname: "Shrivastava" });
+});
+
+app.post("/user", (req, res) => {
+  res.send("Data saved Sucessfully");
+});
+
+app.delete("/user", (req, res) => {
+  res.send("Data deleted Sucessfully");
+});
+
 app.use("/test", (req, res) => {
   res.send("Hello from test ExpressJS");
 });
 
-app.use("/hello", (req, res) => {
-  res.send("Hello Hello!");
-});
+// app.use("/hello", (req, res) => {
+//   res.send("Hello Hello!");
+// });
 
-app.use("/hello", (req, res) => {
-  res.send("Hello Hello!");
-});
-
-app.use((req, res) => {
-  res.send("Hello from ExpressJS");
-});
+// app.use((req, res) => {
+//   res.send("Hello from ExpressJS");
+// });
 
 app.listen(3000, () => {
   console.log("Server successfully created");
